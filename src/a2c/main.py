f32b0397ca16b80839133d92c0e73e2fb07aeda1
Ryan Partridge <rpartridge101@yahoo.co.uk>
2022-04-17 22:57:33 +0100

    Updated main.py
    Moved run to tuner class

--- src/a2c/main.py
+++ src/a2c/main.py
@@ -3,6 +3,7 @@ import argparse
 from a2c.model import ACNetwork
 from a2c.agent import A2C
 from core.experience import FirstLastExpBuffer
+from core.tuning import Tuning
 from core.utils import set_device, set_multi_processors
 
 from nes_py.wrappers import JoypadSpace
@@ -27,21 +28,11 @@ CLIP_GRAD = 0.1
 # Steps ahead to approx reward for every action
 REWARD_STEPS = 4
 
-def parser_settings():
+def parser_settings() -> argparse.Namespace:
     """Enables argument parsing in console and returns passed arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-eps", "--episode_count", required=True, help="Episode count (default=5000)", type=int, default=5000)
     return parser.parse_args()
-
-def run(episode_count: int) -> None:
-    done = True
-    for step in range(episode_count):
-        if done:
-            state = env.reset()
-        state, reward, done, info = env.step(env.action_space.sample()) # pick random action on each step, no learning
-        env.render() # Disable for faster execution (e.g. don't need to render the game output).
-        # If dependencies correctly setup, you shuold see a window pop-up with the Super Mario Bros world in it.
-    env.close()
 
 def main() -> None:
     """Runs the main application."""
@@ -50,10 +41,7 @@ def main() -> None:
     device = set_device()
 
     # Set core classes
-    net = ACNetwork(
-        input_shape=env.observation_space.shape,
-        n_actions=env.action_space.n
-    ).to(device)
+    net = ACNetwork(input_shape=env.observation_space.shape, n_actions=env.action_space.n).to(device)
 
     agent = A2C(
         gamma=GAMMA, 
@@ -62,16 +50,20 @@ def main() -> None:
         device=device
     )
 
-    exp_buffer = FirstLastExpBuffer(
-        env=env,
-        agent=agent,
-        gamma=GAMMA,
-        buffer_size=REWARD_STEPS
-    )
+    exp_buffer = FirstLastExpBuffer(env=env, agent=agent, gamma=GAMMA, buffer_size=REWARD_STEPS)
 
     optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE, eps=EPSILON)
 
-    run(args.episode_count)
+    tuner = Tuning(env=env, agent=agent, model=net, exp_buffer=exp_buffer, optimizer=optimizer, device=device)
+
+    # Train model
+    tuner.train(
+        episode_count=args.episode_count,
+        batch_size=BATCH_SIZE,
+        entropy_beta=ENTROPY_BETA,
+        clip_grad=CLIP_GRAD
+    )
+    # tuner.validate()
 
 if __name__ == "__main__":
     main()
