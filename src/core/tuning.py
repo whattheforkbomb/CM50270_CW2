ff9cb2f5ee565c2dee3d74c53e89590db106136c
Ryan Partridge <rpartridge101@yahoo.co.uk>
2022-04-18 19:33:58 +0100

    Added save and load model
    + amended train function to store essential information for testing and plotting

--- src/core/tuning.py
+++ src/core/tuning.py
@@ -12,24 +12,41 @@ import torch.nn.functional as F
 import torch.nn.utils as nn_utils
 
 class Tuning():
-    """A class dedicated to training and tuning models."""
-    def __init__(self, env: gym.Env, agent: BaseAgent, model: nn.Module, exp_buffer: ExperienceBuffer, optimizer: torch.optim, device: str, stop_reward: int = 30) -> None:
-        self.env = env
+    """
+    A class dedicated to training and tuning models.
+    
+    Parameters:
+    - agent (BaseAgent) - the agent to use for unpacking batches
+    - model (nn.Module) - the model used to train
+    - exp_buffer (ExperienceBuffer) - experience buffer for getting data samples
+    - optimizer (torch.optim) - optimizer used for training the model
+    - stop_reward (int) - mean reward obtained to stop training
+    """
+    def __init__(self, agent: BaseAgent, model: nn.Module, exp_buffer: ExperienceBuffer, optimizer: torch.optim, stop_reward: int = 30) -> None:
         self.agent = agent
         self.model = model
         self.buffer = exp_buffer
         self.optimizer = optimizer
-        self.device = device
         self.reward_threshold = stop_reward
 
-        self.start_time = time.time()
         self.total_frames = 0
         self.total_rewards = []
 
-    def train(self, episode_count: int, batch_size: int, entropy_beta: float, clip_grad: float) -> None:
-        """Train the model on a given number of episodes."""
+    def train(self, episode_count: int, batch_size: int, entropy_beta: float, clip_grad: float, filename: str) -> None:
+        """
+        Train the model on a given number of episodes.
+        
+        Parameters:
+        - episode_count (int) - number of episodes to train the model on
+        - batch_size (int) - size of action probabilities
+        - entropy_beta (float) - metric for entropy loss
+        - clip_grad (float) - metric for preventing large gradients
+        - filename (str) - filename for saving the model
+        """
         done = False
         batch = []
+        losses_policy, losses_sv, losses_entropy, losses_total = [], [], [], []
+        advantages, state_values = [], []
         for i_episode in range(episode_count):
             print(f'({i_episode}/{episode_count})' , end="")
             for step_idx, exp in enumerate(self.buffer):
@@ -37,8 +54,19 @@ class Tuning():
 
                 # Stop training if reached reward threshold
                 new_rewards = self.buffer.pop_total_rewards()
-                if new_rewards and self.reward_tracker(new_rewards[0], step_idx):
-                    save_model()
+                if self.reward_tracker(new_rewards[0], step_idx):
+                    params_dict = {
+                        'parameters': self.model.parameters(),
+                        'advantages': advantages,
+                        'state_values': state_values,
+                        'rewards': self.total_rewards,
+                        'losses_policy': losses_policy,
+                        'losses_sv': losses_sv,
+                        'losses_entropy': losses_entropy,
+                        'losses_total': losses_total,
+                        'policy': policy
+                    }
+                    save_model(model=self.model, filename=filename, params_dict=params_dict)
                     done = True
                     break
 
@@ -53,29 +81,37 @@ class Tuning():
                 
                 # Convert to normalised form
                 log_probs = F.log_softmax(policy, dim=1)
-                temp = rewards - state_value.squeeze(-1).detach()
-                log_action_probs = temp * log_probs[range(batch_size), actions]
+                action_advantage = rewards - state_value.squeeze(-1).detach()
+                log_action_probs = action_advantage * log_probs[range(batch_size), actions]
                 loss_policy = log_action_probs.mean()
                 probs = F.softmax(policy, dim=1)
-                entropy_loss = entropy_beta * (probs * log_probs).sum(dim=1).mean()
+                loss_entropy = entropy_beta * (probs * log_probs).sum(dim=1).mean()
 
                 # Backpropagate policy gradients
                 loss_policy.backward(retain_graph=True)
             
                 # Backpropagate entropy and state-value gradients
-                total_loss = entropy_loss + loss_sv
-                total_loss.backward()
+                loss_total = loss_entropy + loss_sv
+                loss_total.backward()
                 nn_utils.clip_grad_norm_(self.model.parameters(), clip_grad)
                 self.optimizer.step()
 
                 # Get total loss
-                total_loss += loss_policy
-                print(f'state loss {loss_sv:.3f}, policy loss {loss_policy:.3f}, train loss {total_loss:.3f}')
+                loss_total += loss_policy
+                print(f'state loss {loss_sv:.3f}, policy loss {loss_policy:.3f}, train loss {loss_total:.3f}')
+
+                # Append items to lists
+                advantages.append(action_advantage)
+                state_values.append(state_value)
+                losses_policy.append(loss_policy)
+                losses_sv.append(loss_sv)
+                losses_entropy.append(loss_entropy)
+                losses_total.append(loss_total)
             
             # Terminate if training complete
             if done:
                 break
-        self.env.close()
+
         print('Training complete.')
 
     def reward_tracker(self, reward: int, frame: int) -> bool:
@@ -94,7 +130,7 @@ class Tuning():
         if mean_reward > self.reward_threshold:
             print(f"Solved in {frame} frames!")
             return True
-        return False
+        return False        
 
     def validate(self):
         """Test the trained model."""
--- src/core/utils.py
+++ src/core/utils.py
@@ -2,6 +2,7 @@
 Utility functions that are usable across the whole application.
 """
 import torch
+import torch.nn as nn
 
 def set_device() -> str:
     """
@@ -15,14 +16,34 @@ def set_device() -> str:
         print("CUDA unavailable. Device set to CPU.")
     return device
 
+def save_model(params_dict: dict, filename: str) -> None:
+    """
+    Saves a models parameters with the given filepath.
+
+    Parameters:
+    - params_dict (dict) - additional parameters to store
+    - filename (str) - name of the saved model
+    """
+    torch.save(params_dict, f'saved_models/{filename}.pt')
+
+def load_model(model: nn.Module, filename: str) -> None:
+    """
+    Loads a saved model based on the given filepath and stores its parameters to the model.
+
+    Parameters:
+    - model (nn.Module) - model to store parameters to
+    - filename (str) - name of the model to load    
+    """
+    checkpoint = torch.load(f'saved_models/{filename}.pt')
+
+    # Store variables as model attributes
+    for key, val in checkpoint.items():
+        if key != 'parameters':
+            setattr(model, key, val)
+
+    # Load model parameters
+    model.load_state_dict(checkpoint['parameters'])
+
 def set_multi_processors(x: torch.Tensor, y: torch.Tensor) -> None:
     """Splits data across multiple GPU cores."""
-    pass
-
-def save_model():
-    """Saves a models parameters with the given filepath."""
-    pass
-
-def load_model():
-    """Loads a saved model based on the given filepath."""
     pass
