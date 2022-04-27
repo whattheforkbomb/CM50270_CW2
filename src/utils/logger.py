b000b9d29ed5d80c46f9d2dd42152f229c751f38
Ryan Partridge <rpartridge101@yahoo.co.uk>
2022-04-27 12:22:21 +0100

    Added variables and methods

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,8 +1,18 @@
 class Logger():
     """Stores information for each episode iteration."""
     def __init__(self):
-        pass
+        self.actions = []
+        self.avg_returns = []
+        self.total_losses = []
 
-    def add(self, item: dict) -> None:
-        """Add information to the logger."""
-        pass
+    def add_actions(self, actions: list) -> None:
+        """Add episode actions to the logger."""
+        self.actions.append(actions)
+
+    def add_return(self, avg_return: float) -> None:
+        """Add the average return for the episode to the logger."""
+        self.avg_returns.append(avg_return)
+
+    def add_loss(self, total_loss: float) -> None:
+        """Add the total loss of an episode to the logger."""
+        self.total_losses.append(total_loss)
